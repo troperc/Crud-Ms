f020b1c9e56d7f7061d841ae483d533c7e684fa0
troperc <camilo.wxw@hotmail.com>
2023-02-03 21:25:49 +0100

    exceptions

---
+++ src/main/java/com/tutorial/crudmongoback/CRUD/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.tutorial.crudmongoback.CRUD.controller;
+
+import com.tutorial.crudmongoback.CRUD.dto.ProductDto;
+import com.tutorial.crudmongoback.CRUD.entity.Product;
+import com.tutorial.crudmongoback.CRUD.service.ProductService;
+import com.tutorial.crudmongoback.global.exceptions.AttributeException;
+import com.tutorial.crudmongoback.global.exceptions.ResourceNotFoundExceptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return ResponseEntity.ok(productService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getOne(@PathVariable("id") int id) throws ResourceNotFoundExceptions {
+        return ResponseEntity.ok(productService.getOne(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> save(@RequestBody ProductDto dto) throws AttributeException {
+        return ResponseEntity.ok(productService.save(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable("id") int id, @RequestBody ProductDto dto) throws ResourceNotFoundExceptions, AttributeException {
+        return ResponseEntity.ok(productService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathVariable("id") int id) throws ResourceNotFoundExceptions {
+        return ResponseEntity.ok(productService.delete(id));
+    }
+}
---
+++ src/main/java/com/tutorial/crudmongoback/CRUD/service/ProductService.java
@@ -0,0 +1,58 @@
+package com.tutorial.crudmongoback.CRUD.service;
+
+import com.tutorial.crudmongoback.CRUD.dto.ProductDto;
+import com.tutorial.crudmongoback.CRUD.entity.Product;
+import com.tutorial.crudmongoback.CRUD.repository.ProductRepository;
+import com.tutorial.crudmongoback.global.exceptions.AttributeException;
+import com.tutorial.crudmongoback.global.exceptions.ResourceNotFoundExceptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    public Product getOne(int id) throws ResourceNotFoundExceptions {
+        return productRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundExceptions("not found"));
+    }
+
+    public Product save(ProductDto dto) throws AttributeException {
+        if(productRepository.existsByName(dto.getName()))
+            throw new AttributeException("name already in use");
+        int id = autoIncrement();
+        Product product = new Product(id, dto.getName(), dto.getPrice());
+        return productRepository.save(product);
+    }
+
+    public Product update(int id, ProductDto dto) throws ResourceNotFoundExceptions, AttributeException {
+        Product product = productRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundExceptions("not found"));
+        if(productRepository.existsByName(dto.getName()) && productRepository.findByName(dto.getName()).get().getId() != id)
+            throw new AttributeException("name already in use");
+        product.setName(dto.getName());
+        product.setPrice(dto.getPrice());
+        return productRepository.save(product);
+    }
+
+    public Product delete(int id) throws ResourceNotFoundExceptions {
+        Product product = productRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundExceptions("not found"));
+        productRepository.delete(product);
+        return product;
+    }
+
+    // private methods
+    private int autoIncrement() {
+        List<Product> products = productRepository.findAll();
+        return products.isEmpty()? 1 :
+                products.stream().max(Comparator.comparing(Product::getId)).get().getId() + 1;
+    }
+
+}
